ed5e121c9e7eebdba4165e56f3f69ffb231d782e
SkyBaseVoidWare <MinecraftBezNarezok1488@mail.ru>
2026-06-03 14:00:10 +0300

    VoidWare.lua

--- VoidWare.lua
+++ VoidWare.lua
@@ -1,18 +1,30 @@
-local imageUrl = "https://img10.reactor.cc/pics/post/full/%D0%AD%D1%80%D0%BE%D1%82%D0%B8%D0%BA%D0%B0-%D1%81%D0%B8%D1%81%D1%8C%D0%BA%D0%B8-%D0%B1%D0%BE%D1%8F%D0%BD-%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80-%D0%A2%D0%B0%D0%BB%D1%8E%D0%BA%D0%B0-%28%D0%A4%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%29-4026241.png"
-local fileName = "Test1.png"
+-- Создаем основу для интерфейса в защищенной зоне Roblox
+local screenGui = Instance.new("ScreenGui")
+screenGui.Name = "FullScreenImageGui"
+-- Привязываем к CoreGui, чтобы обычные скрипты игры не могли его удалить
+screenGui.Parent = game:GetService("CoreGui") 
+screenGui.IgnoreGuiInset = true -- Игнорируем безопасную зону (полосу сверху)
 
-local success, imageData = pcall(function() return game:HttpGet(imageUrl, true) end)
+-- Создаем саму картинку
+local imageLabel = Instance.new("ImageLabel")
+imageLabel.Name = "MainImage"
+imageLabel.Parent = screenGui
 
-if success then
-    writefile(fileName, imageData)
-    
-    pcall(function()
-        writefile("../../../Pictures/" .. fileName, imageData)
-    end)
-    
-    local gui = Instance.new("ScreenGui", game:GetService("CoreGui"))
-    gui.IgnoreGuiInset = true
-    local img = Instance.new("ImageLabel", gui)
-    img.Size = UDim2.new(1, 0, 1, 0)
-    img.Image = getcustomasset(fileName)
-    img.ScaleType = Enum.ScaleType.Stretch
+-- Растягиваем на 100% ширины и высоты экрана
+imageLabel.Size = UDim2.new(1, 0, 1, 0)
+imageLabel.Position = UDim2.new(0, 0, 0, 0)
+
+-- Убираем задний фон и рамки
+imageLabel.BackgroundTransparency = 1
+imageLabel.BorderSizePixel = 0
+
+-- Ссылка на саму картинку (Замени ID на свой, если нужно)
+-- Сейчас стоит стандартная заглушка
+imageLabel.Image = "rbxassetid://12345678" 
+
+-- Метод растягивания: 
+-- Enum.ScaleType.Stretch - грубо растянуть (картинка может исказиться)
+-- Enum.ScaleType.Crop - заполнить экран с сохранением пропорций (обрежет края)
+imageLabel.ScaleType = Enum.ScaleType.Stretch
+
+print("Картинка успешно выведена на весь экран!")
